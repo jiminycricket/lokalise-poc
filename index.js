2a4f775dff73045fb160f636304d54ff633f6c59
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2019-12-10 14:27:39 +0800

    feat: integrate with preact

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+import { Component, h, render } from '/web_modules/preact.js';
+import { useEffect, useState } from '/web_modules/preact/hooks.js';
+import htm from '/web_modules/htm.js';
+
 import i18next from '/web_modules/i18next.js';
 import XHR from '/web_modules/i18next-xhr-backend.js';
 import LanguageDetector from '/web_modules/i18next-browser-Languagedetector.js';
 
-i18next.use(LanguageDetector).use(XHR).init({
-    backend: {
-        loadPath: '/locales/{{lng}}/pasted.json',
-    },
-    debug: true,
-}).then((t) => {
-    console.log(i18next.t('welcome'));
-})
+const html = htm.bind(h);
+
+function App(props) {
+    const [greeting, setGreeting] = useState('');
+
+    async function init() {
+        await i18next.use(LanguageDetector).use(XHR).init({
+            backend: {
+                loadPath: '/locales/{{lng}}/pasted.json',
+            },
+            debug: true,
+        });
+        setGreeting(i18next.t('welcome'));
+    }
+
+    useEffect(() => init(), []);
+
+    return html`<div>${greeting}</div>`;
+}
+
+const app = html`<${App} />`;
+render(app, document.getElementById('app'));
